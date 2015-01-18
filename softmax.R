a2324aa90e399037b1a635e56aecaa6ef57fff91
Chris Zhang <chris@pebbledata.com>
2015-01-17 23:22:37 -0800

    softmax exercise

---
+++ softmax.R
@@ -0,0 +1,90 @@
+library(R.matlab)
+library(functional)
+library(pracma)
+library(lbfgs)
+library(Matrix)
+library(R.utils)
+
+source("common.R")
+
+loadimages <- function(file) {
+	h <- file(file, "rb")
+	readBin(h, integer(), n=1, endian="big")
+	n_images <- readBin(h, integer(), n=1, endian="big")
+	row <- readBin(h, integer(), n=1, endian="big")
+	col <- readBin(h, integer(), n=1, endian="big")
+	cat("nimages = ", n_images, " row = ", row, " col = ", col, "\n")
+	IMAGES <- matrix(ncol = row * col, nrow = n_images)
+	for (i in 1:n_images) {
+		m <- matrix(readBin(h, integer(), n=row*col, size=1, endian="big", signed = FALSE), nrow = row, byrow = TRUE) / 255
+		IMAGES[i,] <- (as.vector(m))
+	}
+	close(h)
+	IMAGES
+}
+
+loadlabels <- function(file) {
+	h <- file(file, "rb")
+	readBin(h, integer(), n=1, endian="big")
+	n_labels <- readBin(h, integer(), n=1, endian="big")
+	LABELS <- readBin(h, integer(), n=n_labels, size=1, endian="big", signed = FALSE)
+
+	close(h)
+	LABELS
+}
+
+# ret = 1 => J(theta), ret = 2 => grad(J(theta))
+softmax_cost <- function(theta, k, n, lambda, x, y, ret) {
+	m <- nrow(x)
+	t <- matrix(theta, nrow = k)
+	a <- t %*% t(x)
+	a <- exp(t(t(a) - apply(a, 2, max)))
+	a <- t(t(a) / colSums(a))
+	Y <- sparseMatrix(y, 1:m)
+	if (ret == 1)
+		# J(theta)
+		-1/m * sum(log(colSums(Y * a))) + lambda / 2 * sum(t ^ 2)
+	else
+		# grad(J(theta))
+		as.vector(-1/m * ((Y - a) %*% x) + lambda * t)
+}
+
+predict <- function (theta, k, x) {
+	t <- matrix(theta, nrow = k)
+	a <- t %*% t(x)
+	a <- exp(t(t(a) - apply(a, 2, max)))
+	htheta <- t(t(a) / colSums(a))
+	apply(htheta, 2, which.max)
+}
+
+train <- function () {
+	numclasses <- 10
+	inputsize <- 28 * 28
+	lambda <- 1e-4
+	images <- loadimages("train-images-idx3-ubyte")
+	labels <- loadlabels("train-labels-idx1-ubyte")
+	labels <- sapply(labels, function(x) { if (x == 0) 10 else x })
+	range <- 1:nrow(images)
+	images <- images[range,]
+	labels <- labels[range]
+
+	theta <- runif(numclasses * inputsize, 0, 0.005)
+	#grad <- softmax_cost(theta, numclasses, inputsize, lambda, images, labels, 2)
+ 	#compute_numerical_gradient(Curry(softmax_cost,
+ 	#    k = numclasses, n = inputsize, lambda = lambda, x = images, y = labels, ret = 1),
+	#    theta, grad)
+ 	opt <- optim(theta,
+	             Curry(softmax_cost,
+	                   k = numclasses, n = inputsize, lambda = lambda, x = images,
+	                   y = labels, ret = 1),
+	             Curry(softmax_cost,
+	                   k = numclasses, n = inputsize, lambda = lambda, x = images,
+	                   y = labels, ret = 2),
+  		     method = "L-BFGS-B", control= c(trace = 1, maxit=400))
+	images <- loadimages("t10k-images-idx3-ubyte")
+	labels <- loadlabels("t10k-labels-idx1-ubyte")
+	labels <- sapply(labels, function(x) { if (x == 0) 10 else x })
+	pred <- predict(opt$par, numclasses, images)
+	printf("%0.3f%%\n", sum(pred == labels) / length(labels) * 100)
+	opt
+}
